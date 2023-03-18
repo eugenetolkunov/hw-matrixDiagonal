975399de5e7a4f9eead2ec5032690c04d7624616
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-18 15:44:04 +0200

    Complete MatrixDiagonal

--- src/main/java/mate/academy/MatrixDiagonal.java
+++ src/main/java/mate/academy/MatrixDiagonal.java
@@ -1,26 +1,29 @@
 package mate.academy;
 
+import java.util.Arrays;
+
 public class MatrixDiagonal {
     public static void main(String[] args) {
         int[][] matrixTest = {{0, 1, 2},
                               {4, 5, 6},
                               {1, 1, 1}};
 
-        getDiagonal(matrixTest);
+        System.out.println(Arrays.toString(getDiagonal(matrixTest)));
+        System.out.println(Arrays.toString(getCounterDiagonal(matrixTest)));
     }
 
     public static int[] getDiagonal(int[][] matrix) {
-        int[] returnInt = new int[3];
-        for (int i = 0; i < matrix.length; i++) {
+        int[] returnInt = new int[matrix.length];
+        for(int i = 0; i < matrix.length; i++) {
             returnInt[i] = matrix[i][i];
         }
         return returnInt;
     }
 
-    public int[] getCounterDiagonal(int[][] matrix) {
-        int[] returnInt = new int[3];
+    public static int[] getCounterDiagonal(int[][] matrix) {
+        int[] returnInt = new int[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            returnInt[i] = matrix[i][matrix[i].length - i];
+            returnInt[i] = matrix[i][matrix.length-1-i];
         }
         return returnInt;
     }
